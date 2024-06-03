fa1b22d152d3be0acc8955541a5d136f59c67084
andrewhle <andrewle0915@gmail.com>
2024-06-03 13:11:36 -0400

    update

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -31,7 +31,7 @@ function Header() {
           </p>
           <div className="flex items-center space-x-4">
             <Link
-              href="https://drive.google.com/file/d/1r9VnM2lK2y0FY3Ht26Fk6es5XiZV7deq/view?usp=sharing"
+              href="https://drive.google.com/file/d/1nj8Xh35VglODWGlSlKH_fxoYLVdvSNrJ/view?usp=sharing"
               rel="noopener noreferrer"
               target="_blank"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-orange-400 to-yellow-900 hover:bg-cyan-900 text-white mt-3"
